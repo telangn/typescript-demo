d928ce7037eb4e9baa23fc1c614716dcf2da93e9
Ninad Telang <ninad.telang@pgi.com>
2019-05-30 13:20:06 -0700

    Initial Commit

---
+++ servers/server-demo-simple.js
@@ -0,0 +1,10 @@
+const http = require('http');
+http.createServer(function (request, response) {
+  // Send HTTP headers and body with status 200 (meaning success)
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end(`
+    <html><body>
+      <h1>Hello, world!</h1>
+      You asked for: ${request.url}
+    </body></html>`);
+}).listen(1234);
