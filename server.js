33f8d3898f82bb4f5fb21b41873769180a47d9a4
Ninad Telang <ninad.telang@pgi.com>
2019-06-04 14:41:04 -0700

    update

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+const mongoose = require('mongoose');
+/* 
+Command line:
+sudo service mongodb start
+Type 'mongo' to enter shell
+
+others:
+sudo service mongodb status
+sudo service mongodb restart
+sudo service mongodb stop
+*/
+
+// Connect to mongodb
+mongoose.connect('mongodb://localhost/mydb-new', { useNewUrlParser: true });
+mongoose.connection.once('open', function() {
+    console.log('Mongodb - Connection has been made');
+}).on('error', function(error) {
+    console.log('Connection Error: ', error);
+});
+
 const express = require('express');
 const app = express();
 app.use('/node_modules', express.static('node_modules'));
